54705e48e1aa3293543f6d2fb2f9605821973500
Unknown <bradleyah02@gmail.com>
2018-03-30 08:09:54 -0400

    Convert StraightDriveCommand to PID-enabled
    Enable PID controller on StraightDriveCommand for drift correction

--- src/main/java/xyz/nasaknights/powerup/commands/StraightDriveCommand.java
+++ src/main/java/xyz/nasaknights/powerup/commands/StraightDriveCommand.java
@@ -1,41 +1,58 @@
 package xyz.nasaknights.powerup.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.PIDCommand;
 import xyz.nasaknights.powerup.Robot;
 
-public class StraightDriveCommand extends Command {
+public class StraightDriveCommand extends PIDCommand {
     private long timeout;
     private long startMillis = Long.MAX_VALUE;
     private double power;
+    private double angle;
 
     public StraightDriveCommand(double power, long timeout) {
+        super("Straight Drive", .007, 0, .0001);
         this.power = power;
         this.timeout = timeout;
+
+        this.getPIDController().setOutputRange(0, .1);
+        this.getPIDController().setAbsoluteTolerance(2);
     }
 
     @Override
     protected void initialize() {
-        startMillis = System.currentTimeMillis();
-    }
+        angle = Robot.getNavX().getAngle();
 
-    @Override
-    protected void execute() {
-        Robot.getDrivetrain().getDrive().arcadeDrive(power, 0);
+        this.getPIDController().setSetpoint(angle);
+        this.getPIDController().enable();
+
+        startMillis = System.currentTimeMillis();
     }
 
     @Override
     protected void interrupted()
     {
+        this.getPIDController().disable();
         Robot.getDrivetrain().getDrive().arcadeDrive(0, 0);
     }
 
     @Override
     protected void end() {
+        this.getPIDController().disable();
         Robot.getDrivetrain().getDrive().arcadeDrive(0, 0);
     }
 
     @Override
     protected boolean isFinished() {
         return System.currentTimeMillis() > (startMillis + timeout);
+    }
+
+    @Override
+    protected double returnPIDInput() {
+        return Robot.getNavX().getAngle();
+    }
+
+    @Override
+    protected void usePIDOutput(double output) {
+        Robot.getDrivetrain().getDrive().arcadeDrive(power, output);
     }
 }
